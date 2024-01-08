6f6c53cfdb42defd0e24f91a26cf1aebe9a13ed3
Redrikl <124083799+Redrikl@users.noreply.github.com>
2024-01-09 01:06:56 +0300

    Переделал на Vue Modal и Form

--- src/components/ModalApp.vue
+++ src/components/ModalApp.vue
@@ -1,11 +1,7 @@
 <template>
-  <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog modal__width">
-      <div class="modal-content modal__content">
-        <AppForm></AppForm>
-      </div>
-    </div>
-  </div>
+  <b-modal id="exampleModal" title="Modal Title" :visible="modalVisible" @hide="hideModal">
+    <AppForm />
+  </b-modal>
 </template>
 
 <script>
@@ -13,20 +9,18 @@ import AppForm from "@/components/AppForm.vue";
 
 export default {
   components: { AppForm },
+  data() {
+    return {
+      modalVisible: false,
+    };
+  },
+  methods: {
+    showModal() {
+      this.modalVisible = true;
+    },
+    hideModal() {
+      this.modalVisible = false;
+    },
+  },
 };
 </script>
-
-<style>
-.modal__content {
-  padding: 40px;
-  background: #040613 !important;
-}
-.modal__width {
-  width: 700px !important;
-}
-@media (max-width: 440px) {
-  .modal__width {
-    width: 375px !important;
-  }
-}
-</style>
